66f5cf8239a65f45d4174621909da4e07cdbb3ad
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-01-19 23:42:38 -0800

    copied backend files only from planner-app and deleted unnecessary scripts

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const PORT = process.env.PORT || 4000;
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+// Define middleware here
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("Client/build"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
+  });
+}
+// connect to Mongo DB
+mongoose.connect(
+  // TODO- set up mlab on heroku and replace credentials below
+  process.env.MONGODB_URI || "mongodb://localhost/todo" ,
+  { useNewUrlParser: true }
+);
+
+// connect and use router
+const routes = require("./Routes/routes");
+app.use("/", routes);
+// app.use("/api", routes);
+
+//PORT list ening on
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
